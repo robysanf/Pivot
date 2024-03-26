12ad53ed33fba531a5f85482e726d4e33a7df7b6
sanfelici <roberto.sanfelici@gmail.com>
2024-03-26 10:34:49 +0100

    corretto path per deploy

--- setupWDD/deploy.bat
+++ setupWDD/deploy.bat
@@ -5,5 +5,5 @@ if "%arg1%" == "p" call cordova platform add android
 set arg2=%2
 if "%arg2%" == "b" call cordova build android
 echo "install"
-call adb install -r E:\backup\progetti\artfer\setupWDD\platforms\android\app\build\outputs\apk\debug\app-debug.apk
+call adb install -r E:\WDD\Repository\Pivot\setupWDD\platforms\android\app\build\outputs\apk\debug\app-debug.apk
 
